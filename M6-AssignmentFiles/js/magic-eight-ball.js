e0086aab87f11c063a63f31500645d48b2f93266
Alyna Shepherd <alynashepherd@gmail.com>
2026-09-24 14:40:01 -0400

    Developed Magic 8 Ball Application

---
+++ M6-AssignmentFiles/js/magic-eight-ball.js
@@ -0,0 +1,32 @@
+// Put your JavaScript code in this file
+
+// array with 6 possible magic 8 ball answers
+let answers = ["Most likely", "Yes", "Ask Again Later", "Reply Hazy, Try Again", "Very Doubtful", "Don't Count On It"]
+
+// displays magic 8 ball answers
+function displayAnswer() {
+    let index = Math.floor(Math.random() * answers.length);// selects random number to index answers array
+    document.getElementById("circle").style.display = "block";// makes the circle div visible
+    document.getElementById("circle").innerHTML = answers[index];// writes the answer in the circle div
+}
+
+
+document.getElementById("ball").addEventListener("mousedown", function (event) { // when holding click on the magic 8 ball image...
+    event.preventDefault();
+
+    // checks if something has been entered into the question field
+    if (document.getElementById("question").value.length == 0) {
+        window.alert("Enter a question to receive an answer."); // alerts the user they need to input a question before shaking the 8 ball
+    }
+    else {
+        displayAnswer(); // shakes the 8 ball and displays the answer
+    }
+
+});
+
+
+document.getElementById("reset").addEventListener("click", function (event) { // when reset button is clicked...
+    event.preventDefault();
+    
+    document.getElementById("circle").style.display = "none"; // makes circle div invisible
+});
--- README.md
+++ README.md
@@ -48,3 +48,6 @@ INPUT value, unitS, unitE
 METRIC CONVERTER (value, unitS, unitE)
 
 END
+
+## Magic Eight Ball
+This application functions like a digital magic 8 ball. You enter a question in the input field, then hold click to shake the magic 8 ball and let go to get your answer. If you attempt to shake the magic 8 ball without asking a question, you will get an alert asking you to enter a question. You can reset the magic 8 ball with the reset button below it.
